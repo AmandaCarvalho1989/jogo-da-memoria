dcce370fedb06815e0327ff10ae6e5a0ea7f40bd
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2022-06-21 21:12:33 -0300

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import questionImage from "./assets/fonts/question.svg";
+import titleImg from "./assets/fonts/title.svg";
+import { cards } from "./cards";
 
-function App() {
+const App = () => {
+  const [flippedCards, setFlippedCards] = useState<any>([]);
+  const [completedCards, setCompletedCards] = useState<any>([]);
+
+  const onFlipCard = (card: any) => {
+    if (!flippedCards.length) {
+      setFlippedCards([card]);
+    } else if (flippedCards.length === 1) {
+      const isAlreadyFlipped = isThisCardAlreadyFlipped(card);
+      if (isAlreadyFlipped) {
+        return;
+      }
+      setFlippedCards([...flippedCards, card]);
+      const firstFlippedCard = flippedCards[0];
+      const isSameCollection = firstFlippedCard.collection === card.collection;
+      if (isSameCollection) {
+        setCompletedCards([...completedCards, firstFlippedCard, card]);
+      }
+
+      setTimeout(() => {
+        setFlippedCards([]);
+      }, 1000);
+      return;
+    } else return;
+  };
+
+  const isThisCardCompleted = (card: any) =>
+    completedCards.find((completed: any) => completed.id === card.id);
+  const isThisCardAlreadyFlipped = (card: any) =>
+    flippedCards.find((flipped: any) => flipped.id === card.id);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <img src={titleImg} alt="jogo da memoria" />
+      <section className="cards">
+        {cards.map((card, idx) => {
+          const isCompleted = isThisCardCompleted(card);
+          const isThisFlipped = isThisCardAlreadyFlipped(card);
+          const isCompletedOrFlipped = isCompleted || isThisFlipped;
+          const shownImage = isCompletedOrFlipped ? card.image : questionImage;
+          return (
+            <div
+              key={idx}
+              id={card.id}
+              className={`card-item ${
+                !isCompletedOrFlipped ? "unflipped" : "flipped"
+              }`}
+              onClick={() => onFlipCard(card)}
+            >
+              <img src={shownImage} alt="teste" />
+            </div>
+          );
+        })}
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/cards.ts
@@ -0,0 +1,57 @@
+import bookImage from "./assets/book.png";
+import coffinImage from "./assets/coffin-1.png";
+import eyeImage from "./assets/eye-1.png";
+import hatImage from "./assets/hat-1.png";
+import pumpkinImage from "./assets/pumpkin-1.png";
+import puppetImage from "./assets/puppet-1.png";
+import strawmanImage from "./assets/strawman-1.png";
+import witchImage from "./assets/witch-1.png";
+import zombbieImage from "./assets/zombbie-1.png";
+
+const collections = [
+  {
+    collection: "book",
+    image: bookImage,
+  },
+  {
+    collection: "eye",
+    image: eyeImage,
+  },
+  {
+    collection: "coffin",
+    image: coffinImage,
+  },
+  {
+    collection: "hat",
+    image: hatImage,
+  },
+  {
+    collection: "pumpkin",
+    image: pumpkinImage,
+  },
+  {
+    collection: "puppet",
+    image: puppetImage,
+  },
+  {
+    collection: "strawman",
+    image: strawmanImage,
+  },
+  {
+    collection: "witch",
+    image: witchImage,
+  },
+  {
+    collection: "zombbie",
+    image: zombbieImage,
+  },
+];
+
+export const cards = collections
+  .map((item) =>
+    Array.from({ length: 2 }, (_, index) => ({
+      id: `${item.collection}-${index + 1}`,
+      ...item,
+    }))
+  )
+  .flat();
